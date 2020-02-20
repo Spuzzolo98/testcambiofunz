d53c9af523ef097267694f1975daa21ae637b960
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2020-02-20 08:32:09 +0100

    20-02-2020
    interazione di oggetti e/o variabili con funzioni di tipo void

---
+++ src/testcambiofunz/Testcambiofunz.java
@@ -0,0 +1,20 @@
+
+
+package testcambiofunz;
+import java.util.Random;
+import java.util.Scanner;
+public class Testcambiofunz 
+{
+
+    public static void cambio(int j[],int n, String[] args)
+    {
+            j[0] = 5;
+            n = 2;
+    }
+    public static void main(String[] args) 
+    {
+        int j[] = new int[100], n=7;
+        cambio(j,n);
+        System.out.println(j[0] +" "+n); 
+    }    
+}
